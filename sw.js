d5b0ab5cf9e2908c56b08db961cd3283a19cf83e
roderick1026-create <roderick1026@gmail.com>
2026-09-10 00:07:46 +0800

    Release V4.0.5.3 purchase and payment stability

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='okinawa-proxy-v4.0.5.2';
+const CACHE_NAME='okinawa-proxy-v4.0.5.3';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
