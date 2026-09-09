b3dbc3e2a9683618f610714043bc05d7c48a54f2
roderick1026-create <roderick1026@gmail.com>
2026-09-09 13:13:03 +0800

    前端 V4.0.0 新增多人採買顧客分配與後端資料保存

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='okinawa-proxy-v3.9.0';
+const CACHE_NAME='okinawa-proxy-v4.0.0';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
